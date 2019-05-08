e9e5fb59f88c564533915fe448d8225f5a827c1b
Karimit <karimit@live.com>
2019-05-08 18:33:30 +0200

    Updates

---
+++ precache-manifest.066dfc590d3ba92b5f3cb537d628d21e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76ed6bba38d76689816ee3e61753cc6",
+    "url": "/fcm2/index.html"
+  },
+  {
+    "revision": "c2956acadb82b4026904",
+    "url": "/fcm2/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "fa7f563295ebe5d3d558",
+    "url": "/fcm2/static/js/2.2e766da8.chunk.js"
+  },
+  {
+    "revision": "c2956acadb82b4026904",
+    "url": "/fcm2/static/js/main.e90a9d4e.chunk.js"
+  },
+  {
+    "revision": "9c5d7322c73d22fda30f",
+    "url": "/fcm2/static/js/runtime~main.aa078e2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fcm2/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f80773b041f3d7222eac0b750aea9eb7.js"
+  "/fcm2/precache-manifest.066dfc590d3ba92b5f3cb537d628d21e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fcm2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
